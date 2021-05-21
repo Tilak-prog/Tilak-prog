1d323bc528fb2dece788fd0a4aa5bb46c2221670
Tilak-prog <83180741+Tilak-prog@users.noreply.github.com>
2021-05-21 17:32:19 +0530

    Delete web directory

--- web/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useAuthenticated } from '../component/AuthReducer/useAuthenticated';
-
-export default function Layout(): JSX.Element | null {
-  useAuthenticated();
-  return <>Index!</>;
-}
